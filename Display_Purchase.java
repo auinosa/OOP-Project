bfb02028207238dd90321dff734ee03b40c242e4
auinosa <101471873+auinosa@users.noreply.github.com>
2022-05-08 21:18:41 +0400

    Update Display_Purchase.java

--- Display_Purchase.java
+++ Display_Purchase.java
@@ -2,9 +2,6 @@ import java.io.*;
 import javax.swing.*;
 public class Display_Purchase extends javax.swing.JFrame {
 
-    /**
-     * Creates new form Display_Purchase
-     */
     public Display_Purchase() {
         initComponents();
     }
