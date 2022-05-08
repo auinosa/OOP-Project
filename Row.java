82e76c8931798f937c5cfb68c716b5b99a12d363
canweskiptothegoodpart <95938515+canweskiptothegoodpart@users.noreply.github.com>
2022-05-08 21:24:12 +0400

    Add files via upload

---
+++ Row.java
@@ -0,0 +1,14 @@
+public class Row {
+  private static int row;
+  public int getRow(){
+      return row;
+  }
+
+  public static void setRow(){
+      row++;
+  }
+
+  public static void setRow(int i){
+      row = i;
+  }
+}
