c11cfe6977950cd7563525049529717388e743f4
canweskiptothegoodpart <95938515+canweskiptothegoodpart@users.noreply.github.com>
2022-05-14 00:00:51 +0400

    Delete Display_Purchase.java

--- Display_Purchase.java
+++
@@ -1,9 +0,0 @@
-@artyom_isakhanyan
-import java.io.*;
-import javax.swing.*;
-public class Display_Purchase extends javax.swing.JFrame {
-
-    public Display_Purchase() {
-        initComponents();
-    }
-}
